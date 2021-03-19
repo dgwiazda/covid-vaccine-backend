b397415d7f3fd2b11a0a32ce30bb224ec2223190
dgwiazda <damiangwiazdaa@wp.pl>
2021-03-19 21:36:19 +0100

    initial plus dbchangelog.xml

---
+++ src/main/java/com/dgwiazda/covidvaccine/CovidVaccineApplication.java
@@ -0,0 +1,13 @@
+package com.dgwiazda.covidvaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVaccineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidVaccineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgwiazda/covidvaccine/CovidVaccineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgwiazda.covidvaccine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidVaccineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
